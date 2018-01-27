91061ed3469938cf101cb87c4125d6c7c9abc1ff
puttylon <guido.ledermann@gmail.com>
2018-01-27 18:24:10 +0100

    WortAnzahl hinzugefügt; aufräumen geht weiter

---
+++ TextAnalyse/WortAnzahl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TextAnalyse
+{
+    public class WortAnzahl
+    {
+        public int Anzahl { get; set; }
+        public string Wort { get; set; }
+
+        public WortAnzahl()
+        {
+        }
+    }
+}
